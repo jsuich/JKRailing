37e8d1c5dd9b3ac547f07544f69b15dbf8a9e99b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-16 15:31:31 -0500

    ReDoing Initial Add of skeleton ; Prev skeleton was rails 4-0, reinitiated as v 3-2-14

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JKRailing::Application.config.secret_token = '740010d4f587bd52cf7588cc314d752c028d96772b836aa97f413cdba3d8231f839a375261dd93ee4dcbea221656e89d74e9655f559f6fd4c14f975cde885bcb'
