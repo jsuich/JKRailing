ca9b51903a137a2c24c9a7f15d83c5a56cdd4ac0
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-16 19:49:20 -0500

    connected routes to views for user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def create
     @user = User.create(params[:user])
-    if @user
+    if @user.id
       session[:user_id] = @user.id
       redirect_to @posts
     else
+      render "session/create"
     end
   end
 
